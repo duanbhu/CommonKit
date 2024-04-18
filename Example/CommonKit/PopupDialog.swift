3707a912bd10e768a3b9f2a3246aecde355e0e15
dbh <310701836@qq.com>
2024-04-18 13:45:55 +0800

    feat: 手机号记录、虚拟号记录

--- Example/CommonKit/PopupDialog.swift
+++ Example/CommonKit/PopupDialog.swift
@@ -72,18 +72,84 @@ class PopupDialog: UIView {
         return label
     }()
     
-    private lazy var actionBackgroundView: UIView = {
-        let label = UIView()
-            
-        return label
+    private lazy var actionStackView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
     }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    var title: String? = nil
+    
+    var attributedTitle: NSAttributedString? = nil
+    
+    var message: String? = nil
+    
+    var attributedMessage: NSAttributedString? = nil
+    
+    init(title: String?, attributedTitle: NSAttributedString? = nil, message: String? = nil, attributedMessage: NSAttributedString? = nil) {
+        self.title = title
+        self.attributedTitle = attributedTitle
+        self.message = message
+        self.attributedMessage = attributedMessage
+        super.init(frame: .zero)
+        makeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func makeUI() {
+        configTitle()
+        
+    }
+    
+    func configTitle() {
+        guard let title = title else { return }
+        titleLabel.text = title
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.widthAnchor.constraint(equalTo: widthAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 55)
+        ])
+    }
+    
+    func configMessage() {
+        guard let message = message else { return }
+        messageLabel.text = title
+        
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            messageLabel.topAnchor.constraint(equalTo: topAnchor),
+            messageLabel.widthAnchor.constraint(equalTo: widthAnchor),
+            messageLabel.heightAnchor.constraint(equalToConstant: 55)
+        ])
+    }
+    
+    func configTextF() {
+        guard let message = message else { return }
+        messageLabel.text = title
+        
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            messageLabel.topAnchor.constraint(equalTo: topAnchor),
+            messageLabel.widthAnchor.constraint(equalTo: widthAnchor),
+            messageLabel.heightAnchor.constraint(equalToConstant: 55)
+        ])
+    }
     
     @discardableResult
     func title(_ style: LabelStyle) -> Self {
-        
         titleLabel.text = style.text
         return self
     }
-    
-    
 }
